e5fd5225963e30312b02a33a4dc5369b2d763410
ManolIuj990 <fl_studio1@hotmail.com>
2024-07-12 20:42:25 -0600

    Add files via upload

---
+++ 03pagina03/js/playlist.js
@@ -0,0 +1,25 @@
+let songs = [{
+        name: "Hay Muchas Formas Instrumental Beats",
+        artist: "FL VALENCIA R",
+        img: "imagenx1",
+        audio: "music1"
+    },
+    {
+        name: "Maravilloso Instrumental Beats",
+        artist: "FL VALENCIA R",
+        img: "imagenx2",
+        audio: "music2"
+    },
+    {
+        name: "Levantate y El Victorioso Instrumental Beats",
+        artist: "FL VALENCIA R",
+        img: "imagenx3",
+        audio: "music3"
+    },
+    {
+        name: "David Danzaba Instrumental Beats",
+        artist: "FL VALENCIA R",
+        img: "imagenx4",
+        audio: "music4"
+    }
+]
